7a4ec2b6ee13948a3c82ee76169f2d228caba13c
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-20 22:58:33 +0100

    main.h

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef HEADER
+#define HEADER
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdarg.h>
+#include <unistd.h>
+int _putchar(char  c);
+int _printf(const char *format, ...);
+#endif
