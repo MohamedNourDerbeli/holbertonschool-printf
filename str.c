b308736b824e74a512b2d35802077e7cf1b59676
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-20 23:39:13 +0100

    string printer

--- str.c
+++ str.c
@@ -2,17 +2,17 @@
 int str(char *c)
 {
 	int i;
-	if (c)
+
+	for (i = 0; c[i] != '\0'; i++)
 	{
-		for (i = 0; c[i] != '\0'; i++)
+		if (c == NULL)
 		{
+			c = "(null)";
 			_putchar(c[i]);
 		}
+		else
+			_putchar(c[i]);
 	}
-	if (c == NULL)
-	{
-		c = "(null)";
-		_putchar(c[i]);
-	}
+
 	return (i);
 }
