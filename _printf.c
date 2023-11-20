1c3520198ff1eef50e40837abfa0d05d5433e827
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2023-11-20 23:24:20 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -6,7 +6,7 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i, byte, s_count;
+	unsigned int i, byte, s_count=0;
 	va_list args;
 	va_start(args, format);
 	if (!format || (format[0] == '%' && format[1] == '\0'))
